81a888e00e6fef8c34dbbabccb7382fb48da7e46
Goose1909 <cep42@students.uwf.edu>
2017-12-16 12:39:06 -0600

    updated contact

--- Contact.cs
+++ Contact.cs
@@ -8,5 +8,61 @@ namespace WindowsFormGUI
 {
     class Contact
     {
+        private string firstName;
+        private string lastName;
+        private string phoneNum;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        public string PhoneNum
+        {
+            get { return phoneNum; }
+            set
+            {
+                if (value.Length == 10)
+                {
+                    phoneNum = value;
+                }
+                else
+                {
+                    phoneNum = "No Phone Set";
+                }
+            }
+        }
+
+        //default constructor
+        public Contact()
+        {
+            firstName = "";
+            lastName = "";
+            phoneNum = "";
+        }
+
+        //param constructor
+        public Contact(string newFirst, string newLast, string newNum)
+        {
+            this.FirstName = newFirst;
+            this.LastName = newLast;
+            this.PhoneNum = newNum;
+        }
+
+        public override string ToString()
+        {
+            string output = String.Empty;
+
+            output += String.Format("{0}, {1}, {2}", this.LastName, this.FirstName, this.PhoneNum);
+            return output;
+        }
+
     }
 }
